0687cfd98f34f5a66fc16da1bc8b7e3572d129a2
Jenia Peimer <jpeimer@redhat.com>
2022-09-06 22:08:18 +0300

    [Storage] VMExport tests: unprivileged client fails to create vmexport
    Depends on: https://github.com/RedHatQE/openshift-python-wrapper/pull/1048
    (Merged)
    
    Change-Id: Icff6e92fbc569264652025ea6025afe612d67377
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/438197
    Tested-by: Jenia Peimer <jpeimer@redhat.com>
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Roni Kishner <rkishner@redhat.com>
    Reviewed-by: Sarah Bennert <sbennert@redhat.com>
    Reviewed-by: Denys Shchedrivyi <dshchedr@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>

---
+++ tests/storage/vm_export/test_vm_export.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+"""
+VMExport tests
+"""
+
+import pytest
+from kubernetes.client import ApiException
+from ocp_resources.resource import Resource
+from ocp_resources.virtual_machine_export import VirtualMachineExport
+from ocp_utilities.infra import cluster_resource
+from pytest_testconfig import config as py_config
+
+from utilities.constants import Images
+
+
+VIRTUALMACHINEEXPORTS = "virtualmachineexports"
+ERROR_MSG_USER_CANNOT_CREATE_VM_EXPORT = (
+    rf".*{VIRTUALMACHINEEXPORTS}.{Resource.ApiGroup.EXPORT_KUBEVIRT_IO} is forbidden: User.*cannot create resource"
+    rf".*{VIRTUALMACHINEEXPORTS}.*in API group.*{Resource.ApiGroup.EXPORT_KUBEVIRT_IO}.*in the namespace"
+)
+
+
+@pytest.mark.parametrize(
+    "data_volume_scope_function",
+    [
+        pytest.param(
+            {
+                "dv_name": "cirros-dv-9338",
+                "image": f"{Images.Cirros.DIR}/{Images.Cirros.QCOW2_IMG}",
+                "dv_size": Images.Cirros.DEFAULT_DV_SIZE,
+                "storage_class": py_config["default_storage_class"],
+            },
+            marks=pytest.mark.polarion("CNV-9338"),
+        )
+    ],
+    indirect=True,
+)
+def test_fail_to_vmexport_with_unprivileged_client_no_permissions(
+    unprivileged_client,
+    data_volume_scope_function,
+):
+    with pytest.raises(
+        ApiException,
+        match=ERROR_MSG_USER_CANNOT_CREATE_VM_EXPORT,
+    ):
+        with cluster_resource(VirtualMachineExport)(
+            name="vmexport-unprivileged",
+            namespace=data_volume_scope_function.namespace,
+            client=unprivileged_client,
+            source_api_group="",
+            source_kind=VirtualMachineExport.SourceKind.PVC,
+            source_name=data_volume_scope_function.name,
+        ) as vmexport:
+            assert not vmexport, "VMExport created by unprivileged client"
