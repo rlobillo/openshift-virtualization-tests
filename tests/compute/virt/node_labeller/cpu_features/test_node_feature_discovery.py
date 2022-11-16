6f3e8db255e2028c6064bed4397925c614c9c20e
Vasiliy Sibirskiy <vsibirsk@redhat.com>
2022-11-16 13:13:33 +0200

    [virt/node_labeler] Remove minCPU related tests
    Now minCPU doesn't affect the labeling nodes with cpu features
    https://bugzilla.redhat.com/show_bug.cgi?id=2086551
    https://github.com/kubevirt/kubevirt/pull/7765
    
    Change-Id: If9664c159dfe4ea60146d4420fa9d7886dba9108
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/435436
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Tested-by: Vasiliy Sibirskiy <vsibirsk@redhat.com>
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Reviewed-by: Denys Shchedrivyi <dshchedr@redhat.com>

--- tests/compute/virt/node_labeller/cpu_features/test_node_feature_discovery.py
+++ tests/compute/virt/node_labeller/cpu_features/test_node_feature_discovery.py
@@ -1,23 +1,13 @@
 """
 Test node feature discovery.
 """
-from xml.etree import ElementTree
-
 import pytest
-from ocp_utilities.infra import cluster_resource
 
 from tests.compute.utils import update_hco_annotations
 from tests.compute.virt.node_labeller.constants import CPU_MODEL_LABEL_PREFIX
-from utilities.virt import (
-    VirtualMachineForTests,
-    fedora_vm_body,
-    running_vm,
-    wait_for_kv_stabilize,
-    wait_for_updated_kv_value,
-)
+from utilities.virt import wait_for_kv_stabilize, wait_for_updated_kv_value
 
 
-MIN_CPU = "minCPUModel"
 OBSOLETE_CPU = "obsoleteCPUModels"
 
 
@@ -56,66 +46,26 @@ def nodes_labels_dict(nodes):
 
 
 @pytest.fixture()
-def libvirt_min_cpu_features_list(
-    request, nodes_common_cpu_model, cpu_test_vm, admin_client
-):
-    """
-    Extract minimal CPU model features from libvirt/cpu_map xml.
-    Use x86_Penryn if request.param["default_min_cpu"] else nodes_common_cpu_model
-    """
-    min_cpu_model = (
-        "Penryn" if request.param["default_min_cpu"] else nodes_common_cpu_model
-    )
-    stdout = cpu_test_vm.vmi.virt_launcher_pod.execute(
-        command=[
-            "cat",
-            f"/usr/share/libvirt/cpu_map/x86_{min_cpu_model}.xml",
-        ]
-    )
-    tree = ElementTree.fromstring(stdout)
-
-    return [
-        feature.get("name") for feature in tree.findall("model")[0].findall("feature")
-    ]
-
-
-@pytest.fixture()
 def updated_kubevirt_cpus(
-    request,
     hyperconverged_resource_scope_function,
     nodes_common_cpu_model,
     admin_client,
     hco_namespace,
 ):
-    is_min_cpu = request.param["cpu_config"] == MIN_CPU  # MIN_CPU or OBSOLETE_CPU
-
-    hco_path = MIN_CPU if is_min_cpu else OBSOLETE_CPU
-    hco_value = nodes_common_cpu_model if is_min_cpu else {nodes_common_cpu_model: True}
-    kv_path = [MIN_CPU] if is_min_cpu else [OBSOLETE_CPU, nodes_common_cpu_model]
-    kv_value = nodes_common_cpu_model if is_min_cpu else True
-
     with update_hco_annotations(
-        resource=hyperconverged_resource_scope_function, path=hco_path, value=hco_value
+        resource=hyperconverged_resource_scope_function,
+        path=OBSOLETE_CPU,
+        value={nodes_common_cpu_model: True},
     ):
         wait_for_updated_kv_value(
             admin_client=admin_client,
             hco_namespace=hco_namespace,
-            path=kv_path,
-            value=kv_value,
+            path=[OBSOLETE_CPU, nodes_common_cpu_model],
+            value=True,
         )
         wait_for_kv_stabilize(admin_client=admin_client, hco_namespace=hco_namespace)
         yield
     wait_for_kv_stabilize(admin_client=admin_client, hco_namespace=hco_namespace)
-
-
-@pytest.fixture()
-def cpu_test_vm(namespace):
-    name = "cpu-test"
-    with cluster_resource(VirtualMachineForTests)(
-        name=name, namespace=namespace.name, body=fedora_vm_body(name=name)
-    ) as vm:
-        running_vm(vm=vm, check_ssh_connectivity=False)
-        yield vm
 
 
 def node_label_checker(node_label_dict, label_list, dict_key):
@@ -143,28 +93,6 @@ def test_obsolete_cpus_in_node_labels(nodes_labels_dict, kubevirt_config):
         dict_key="cpu_models",
     )
     assert not any(test_dict.values()), f"Obsolete CPU found in labels\n{test_dict}"
-
-
-@pytest.mark.parametrize(
-    "libvirt_min_cpu_features_list",
-    [
-        pytest.param(
-            {"default_min_cpu": True},
-            marks=pytest.mark.polarion("CNV-2798"),
-        )
-    ],
-    indirect=True,
-)
-def test_min_cpus_in_node_labels(nodes_labels_dict, libvirt_min_cpu_features_list):
-    """
-    Test min CPU. Min CPU features don't appear in node labels.
-    """
-    test_dict = node_label_checker(
-        node_label_dict=nodes_labels_dict,
-        label_list=libvirt_min_cpu_features_list,
-        dict_key="cpu_features",
-    )
-    assert not any(test_dict.values()), f"Min CPU feature found in labels\n{test_dict}"
 
 
 @pytest.mark.polarion("CNV-3607")
@@ -211,16 +139,7 @@ def test_hardware_non_required_node_labels(nodes_labels_dict):
     ), f"Some nodes have non required KVM labels: {test_dict}"
 
 
-@pytest.mark.parametrize(
-    "updated_kubevirt_cpus",
-    [
-        pytest.param(
-            {"cpu_config": OBSOLETE_CPU},
-            marks=pytest.mark.polarion("CNV-6103"),
-        )
-    ],
-    indirect=True,
-)
+@pytest.mark.polarion("CNV-6103")
 def test_updated_obsolete_cpus_in_node_labels(
     updated_kubevirt_cpus, nodes_labels_dict, kubevirt_config
 ):
@@ -233,28 +152,3 @@ def test_updated_obsolete_cpus_in_node_labels(
         dict_key="cpu_models",
     )
     assert not any(test_dict.values()), f"Obsolete CPU found in labels\n{test_dict}"
-
-
-@pytest.mark.parametrize(
-    "updated_kubevirt_cpus, libvirt_min_cpu_features_list",
-    [
-        pytest.param(
-            {"cpu_config": MIN_CPU},
-            {"default_min_cpu": False},
-            marks=pytest.mark.polarion("CNV-6104"),
-        )
-    ],
-    indirect=True,
-)
-def test_updated_min_cpu_in_node_labels(
-    updated_kubevirt_cpus, nodes_labels_dict, libvirt_min_cpu_features_list
-):
-    """
-    Test user-updated minCPUModel does not appear in node labels.
-    """
-    test_dict = node_label_checker(
-        node_label_dict=nodes_labels_dict,
-        label_list=libvirt_min_cpu_features_list,
-        dict_key="cpu_features",
-    )
-    assert not any(test_dict.values()), f"Min CPU feature found in labels\n{test_dict}"
