eb0b266cbb948da509a8ad7d24dc2152b3417776
Vasiliy Sibirskiy <vsibirsk@redhat.com>
2022-12-08 14:04:41 +0200

    Add BZ 2145223 W/A
    Error message that indicates dv/pvc creation error is missing
    in VM status.
    W/A just checks that VM is not starting without checking for
    a specific error
    
    Change-Id: Ib88ae119b0d475bea44e57a902c84750ef0f684d
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/437355
    Reviewed-by: Meni Yakove <myakove@redhat.com>
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Tested-by: Vasiliy Sibirskiy <vsibirsk@redhat.com>

--- tests/compute/ssp/supported_os/common_templates/golden_images/utils.py
+++ tests/compute/ssp/supported_os/common_templates/golden_images/utils.py
@@ -1,7 +1,6 @@
 import logging
 
 from ocp_resources.utils import TimeoutExpiredError, TimeoutSampler
-from pytest_testconfig import py_config
 
 from utilities.constants import TIMEOUT_2MIN
 
@@ -10,25 +9,21 @@ LOGGER = logging.getLogger(__name__)
 
 
 def assert_missing_golden_image_pvc(vm, pvc_name):
-    def _verify_missing_pvc_in_vm_conditions(_vm, _expected_message):
-        if vm.instance.status.conditions:
+    def _verify_missing_pvc_in_vm_conditions(_conditions, _expected_message):
+        if _conditions:
             return any(
-                [
-                    _expected_message in condition["message"]
-                    for condition in _vm.instance.status.conditions
-                ]
+                [_expected_message in condition["message"] for condition in _conditions]
             )
 
-    expected_message = (
-        f"Source PVC {py_config['golden_images_namespace']}/{pvc_name} not found"
-    )
+    expected_message = "VMI does not exist"
+
     try:
         # Verify VM error on missing source PVC
         for sample in TimeoutSampler(
             wait_timeout=TIMEOUT_2MIN,
             sleep=5,
             func=_verify_missing_pvc_in_vm_conditions,
-            _vm=vm,
+            _conditions=vm.instance.status.conditions,
             _expected_message=expected_message,
         ):
             if sample:
