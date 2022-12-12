fabf2d601ae78dfcbc947cea689cd556841a84cf
akriti gupta <akrgupta@redhat.com>
2022-12-12 16:47:42 +0530

    removing is_bug_open from alerts test
    Signed-off-by: akriti gupta <akrgupta@redhat.com>
    Change-Id: I04d74de819328de89b2a291436e1bc5c425571f1
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/437462
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Dalia Frank <dafrank@redhat.com>
    Reviewed-by: Jenia Peimer <jpeimer@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>

--- tests/compute/virt/alerts/test_virt_pods_alerts.py
+++ tests/compute/virt/alerts/test_virt_pods_alerts.py
@@ -6,7 +6,6 @@ import pytest
 
 from tests.compute.utils import verify_no_listed_alerts_on_cluster
 from utilities.hco import ResourceEditorValidateHCOReconcile
-from utilities.infra import is_bug_open
 
 
 VIRT_ALERTS_LIST = [
@@ -59,9 +58,6 @@ class TestVirtAlerts:
         self,
         prometheus,
     ):
-        if is_bug_open(bug_id=2126397):
-            VIRT_ALERTS_LIST.remove("KubeVirtComponentExceedsRequestedMemory")
-
         verify_no_listed_alerts_on_cluster(
             prometheus=prometheus, alerts_list=VIRT_ALERTS_LIST
         )
