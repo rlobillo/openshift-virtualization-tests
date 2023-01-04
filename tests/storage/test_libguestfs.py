e2192c2325ab8393a902829c7300a8b20beb26e7
kgoldbla <kgoldbla@redhat.com>
2023-01-04 18:15:12 +0200

    [Storage] Adding new --group non-root flag to virtctl guestfs
    Change-Id: Iac02b1b0588c72180d26ee023aa7e4e300dc1c1f
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/438376
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Jenia Peimer <jpeimer@redhat.com>
    Reviewed-by: Sarah Bennert <sbennert@redhat.com>
    Reviewed-by: Denys Shchedrivyi <dshchedr@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Tested-by: Kevin Goldblatt <kgoldbla@redhat.com>

--- tests/storage/test_libguestfs.py
+++ tests/storage/test_libguestfs.py
@@ -1,9 +1,10 @@
 import pexpect
 import pytest
+from ocp_resources.datavolume import DataVolume
 from ocp_resources.pod import Pod
 from pytest_testconfig import config as py_config
 
-from utilities.constants import Images
+from utilities.constants import TIMEOUT_1MIN, Images
 
 
 pytestmark = pytest.mark.post_upgrade
@@ -12,13 +13,13 @@ pytestmark = pytest.mark.post_upgrade
 @pytest.fixture()
 def virtctl_libguestfs(data_volume_scope_function):
     guestfs_proc = pexpect.spawn(
-        f"virtctl guestfs {data_volume_scope_function.name} -n {data_volume_scope_function.namespace}"
+        f"virtctl guestfs {data_volume_scope_function.name} -n {data_volume_scope_function.namespace} --group 2000"
     )
     guestfs_proc.send("\n\n")
-    guestfs_proc.expect("#", timeout=60)
+    guestfs_proc.expect("$", timeout=TIMEOUT_1MIN)
     yield guestfs_proc
     guestfs_proc.sendcontrol(char="d")
-    guestfs_proc.expect(pexpect.EOF, timeout=60)
+    guestfs_proc.expect(pexpect.EOF, timeout=TIMEOUT_1MIN)
     guestfs_proc.close()
     Pod(
         name=f"libguestfs-tools-{data_volume_scope_function.name}",
@@ -36,6 +37,7 @@ def virtctl_libguestfs(data_volume_scope_function):
                 "image": f"{Images.Cirros.DIR}/{Images.Cirros.QCOW2_IMG}",
                 "dv_size": Images.Cirros.DEFAULT_DV_SIZE,
                 "storage_class": py_config["default_storage_class"],
+                "volume_mode": DataVolume.VolumeMode.FILE,
             },
             marks=(pytest.mark.polarion("CNV-6566")),
         ),
@@ -44,4 +46,4 @@ def virtctl_libguestfs(data_volume_scope_function):
 )
 def test_virtctl_libguestfs(data_volume_scope_function, virtctl_libguestfs):
     virtctl_libguestfs.sendline("libguestfs-test-tool")
-    virtctl_libguestfs.expect("===== TEST FINISHED OK =====", timeout=60)
+    virtctl_libguestfs.expect("===== TEST FINISHED OK =====", timeout=TIMEOUT_1MIN)
