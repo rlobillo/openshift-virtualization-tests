72dc4195a779e04ebe5220e258815f8c6292079d
Yossi Segev <ysegev@redhat.com>
2023-06-13 19:04:52 +0300

    Skip MetalLB tests until fixing teardown
    MetalLB tests teardown is followed by cordoning cluster nodes,
    which causes the failure of all the following tests.
    https://issues.redhat.com/browse/CNV-29859 tracks that, and until this
    issue is resolved - MetalLB shouldn't be tested.
    
    Change-Id: I6f9c5bd8c27eb4bb580ffc3e806eb2e72b6367c9
    Reviewed-on: https://code.engineering.redhat.com/gerrit/c/cnv-tests/+/444098
    Tested-by: Yossi Segev <ysegev@redhat.com>
    Continuous-Integration: CNV-QE Gerrit <no-reply-cnv-qe-devops@redhat.com>
    Reviewed-by: Satheesaran Sundaramoorthi <sasundar@redhat.com>
    Reviewed-by: Debarati Basu-Nag <dbasunag@redhat.com>
    Reviewed-by: Anat Wax <awax@redhat.com>
    Reviewed-by: Jenia Peimer <jpeimer@redhat.com>

--- tests/network/metallb/test_metallb_operator_install.py
+++ tests/network/metallb/test_metallb_operator_install.py
@@ -3,6 +3,9 @@ import pytest
 from tests.network.metallb.utils import validate_metallb_pods_running
 
 
+pytestmark = pytest.mark.jira("CNV-29859", run=False)
+
+
 @pytest.mark.polarion("CNV-9574")
 def test_metallb_operator(
     admin_client, created_metallb_namespace, installed_metallb_operator
