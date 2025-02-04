e6478d47968237d91d0dee32141012fb53fa88e1
Jenia Peimer <86722603+jpeimer@users.noreply.github.com>
2025-02-04 15:28:01 +0200

    [4.12] Storage: adjust gating tests (#2664) (#2677)
    - remove test_upload_after_certs_renewal from gating
    - change timeout to posibly reduce flakiness of this test
    
    cherry-pick: https://github.com/RedHatQE/openshift-virtualization-tests/pull/180

--- tests/storage/test_cdi_certificate.py
+++ tests/storage/test_cdi_certificate.py
@@ -30,8 +30,8 @@ from utilities.constants import (
     CDI_SECRETS,
     TIMEOUT_1MIN,
     TIMEOUT_3MIN,
+    TIMEOUT_5SEC,
     TIMEOUT_10MIN,
-    TIMEOUT_20SEC,
     Images,
 )
 from utilities.hco import ResourceEditorValidateHCOReconcile
@@ -165,8 +165,8 @@ def refresh_cdi_certificates(secrets):
                 LOGGER.info(f"Wait for Secret {secret.name} to be updated")
                 res.update()
                 for sample in TimeoutSampler(
-                    wait_timeout=TIMEOUT_20SEC,
-                    sleep=10,
+                    wait_timeout=TIMEOUT_1MIN,
+                    sleep=TIMEOUT_5SEC,
                     func=lambda: secret.certificate_not_before
                     != secret.certificate_not_after,
                 ):
