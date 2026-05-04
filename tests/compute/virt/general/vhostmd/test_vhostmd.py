0b2a99ad78721f878d3f2e8923e7c81896679089
Samuel Alberstein <137210078+SamAlber@users.noreply.github.com>
2026-05-04 17:56:45 +0200

    [4.12] [VIRT] Fix for vhostmd test (#3464)
    Download and install rpm from file-server without using of subsription-manager
    
    Signed-off-by: Samuel Albershtein <salbersh@redhat.com>

--- tests/compute/virt/general/vhostmd/test_vhostmd.py
+++ tests/compute/virt/general/vhostmd/test_vhostmd.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import shlex
 
 import pytest
 import xmltodict
@@ -7,17 +9,32 @@ from pyhelper_utils.shell import run_ssh_commands
 from pytest_testconfig import config as py_config
 
 from tests.os_params import RHEL_LATEST, RHEL_LATEST_LABELS, RHEL_LATEST_OS
-from utilities.constants import RHSM_PASSWD, RHSM_USER, TIMEOUT_3MIN
-from utilities.virt import (
-    prepare_cloud_init_user_data,
-    running_vm,
-    vm_instance_from_template,
-)
+from utilities.constants import TIMEOUT_3MIN
+from utilities.virt import running_vm, vm_instance_from_template
 
 
 pytestmark = pytest.mark.post_upgrade
 
 LOGGER = logging.getLogger(__name__)
+
+VM_DUMP_METRICS_RPM = "vm-dump-metrics.rpm"
+
+
+def download_and_install_vm_dump_metrics(vm):
+    LOGGER.info(f"Download and install vm-dump-metrics tool to VM: {vm.name}")
+    url_path = (
+        f"{py_config['servers']['https_server']}cnv-tests/rpms/{VM_DUMP_METRICS_RPM}"
+    )
+    artifactory_header_string = (
+        f'Authorization: Bearer {os.environ["ARTIFACTORY_TOKEN"]}'
+    )
+    run_ssh_commands(
+        host=vm.ssh_exec,
+        commands=[
+            shlex.split(f"curl {url_path} -H '{artifactory_header_string}' -k -O"),
+            shlex.split(f"sudo yum install -y ./{VM_DUMP_METRICS_RPM}"),
+        ],
+    )
 
 
 @pytest.fixture()
@@ -34,7 +51,6 @@ def vhostmd_vm1(
         namespace=namespace,
         data_source=golden_image_data_source_scope_function,
         node_selector=schedulable_nodes[0].name,
-        cloud_init_data=rhsm_and_vmdumpmetrics_pkg_cloud_init_data(),
     ) as vhostmd_vm1:
         vhostmd_vm1.start()
         yield vhostmd_vm1
@@ -54,7 +70,6 @@ def vhostmd_vm2(
         namespace=namespace,
         data_source=golden_image_data_source_scope_function,
         node_selector=schedulable_nodes[0].name,
-        cloud_init_data=rhsm_and_vmdumpmetrics_pkg_cloud_init_data(),
     ) as vhostmd_vm2:
         vhostmd_vm2.start()
         yield vhostmd_vm2
@@ -62,12 +77,14 @@ def vhostmd_vm2(
 
 @pytest.fixture()
 def running_vhostmd_vm1(vhostmd_vm1):
-    return running_vm(vm=vhostmd_vm1, ssh_timeout=TIMEOUT_3MIN)
+    running_vm(vm=vhostmd_vm1, ssh_timeout=TIMEOUT_3MIN)
+    download_and_install_vm_dump_metrics(vm=vhostmd_vm1)
 
 
 @pytest.fixture()
 def running_vhostmd_vm2(vhostmd_vm2):
-    return running_vm(vm=vhostmd_vm2, ssh_timeout=TIMEOUT_3MIN)
+    running_vm(vm=vhostmd_vm2, ssh_timeout=TIMEOUT_3MIN)
+    download_and_install_vm_dump_metrics(vm=vhostmd_vm2)
 
 
 def run_vm_dump_metrics(vm):
@@ -75,22 +92,6 @@ def run_vm_dump_metrics(vm):
         host=vm.ssh_exec,
         commands=["sudo", "vm-dump-metrics"],
     )[0]
-
-
-def rhsm_and_vmdumpmetrics_pkg_cloud_init_data():
-    bootcmds = [
-        "sudo subscription-manager config --rhsm.auto_enable_yum_plugins=0",
-        (
-            "sudo subscription-manager register "
-            "--serverurl=subscription.rhsm.stage.redhat.com:443/subscription "
-            "--baseurl=https://cdn.stage.redhat.com "
-            f"--username={RHSM_USER} "
-            f"--password={RHSM_PASSWD} "
-            "--auto-attach"
-        ),
-        "sudo yum install -y vm-dump-metrics",
-    ]
-    return prepare_cloud_init_user_data(section="bootcmd", data=bootcmds)
 
 
 @pytest.mark.parametrize(
